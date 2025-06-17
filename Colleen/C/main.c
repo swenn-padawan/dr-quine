2872887cb776ca8f2f7ee76eba080c0177055335
Swenn Tetrel <stetrel@2A7.42angouleme.fr>
2025-06-17 09:49:45 +0200

    🚧 wip: need a \n

--- Colleen/C/main.c
+++ Colleen/C/main.c
@@ -1,7 +1,2 @@
 #include <stdio.h>
-
-int main()
-{
-	char	str[] = "";
-	printf("%s", str);
-}
+int main(){char	str[] = "#include <stdio.h>int main(){char str[] = %c%s%c;printf(str,34,str,34,10);}%c";printf(str,34,str,34,10);}
